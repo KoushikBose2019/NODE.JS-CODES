45ef5e3fd2ff2b94f530f9ef14022e097886ee82
Koushik Bose <48059128+KoushikBose2019@users.noreply.github.com>
2019-06-15 12:45:43 +0530

    My first Express.js app

--- main.js
+++ main.js
@@ -1,26 +1,11 @@
-console.log("hello node.js!!");
+const express= require("express");
+const app=express();
+//console.log(app);
+app.get("/",(req,res)=>{
+    res.send("hello world");
+});
+app.post("/",(req,res)=>{
+    res.send("user data access");
+});
 
-let user={
-    name:"arnok",
-    age:28,
-    email:"arnokgosh2022@gmail.com",
-    address:"dum dum"
-}
-
-console.log(user.name)
-console.log(user)
-//anonymous function
-let person=function(a,b){
-    let c=a+b
-  console.log("addition of a and b is : "+c);
-}
-person(2,4);
-//arrow function
-let add=(a,b)=>{
-    let c=a+b;
-    console.log("addition of the number is :"+c);
-//let c=a+b;
-}
-add(4,7);
-let name="arnok";
-module.exports.abc=name;
+app.listen(3000,()=>console.log("server running on port :3000"));
