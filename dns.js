489779ee993efccc52ec8085de26a040b1836b33
Koushik Bose <48059128+KoushikBose2019@users.noreply.github.com>
2019-06-17 20:45:40 +0530

    dns  module in node.js

---
+++ dns.js
@@ -0,0 +1,12 @@
+var dns = require('dns');
+
+dns.lookup('www.pwc.com', function (err, address) {
+   console.log('address:', address);
+   dns.reverse(address, function (err, hostnames) {
+      if (err) {
+         console.log(err.stack);
+      }
+
+      console.log('reverse for ' + address + ': ' + JSON.stringify(hostnames));
+   });  
+});
