cf8a27240cf5635df2cea2276ecf2ad9f117cbc1
Koushik Bose <48059128+KoushikBose2019@users.noreply.github.com>
2019-07-04 12:16:45 +0530

    mongodb connection

--- mongoose.js
+++ mongoose.js
@@ -1,29 +1,41 @@
-var mongoose= require("mongoose");
-//create a schema
+var mongoose=require('mongoose');
+mongoose.connect('mongodb://localhost:27017/employee', {useNewUrlParser: true});
+var conn=mongoose.connection;
 var employeeSchema=new mongoose.Schema({
     name:String,
     email:String,
     etype:String,
     hourlyrate:Number,
-    totalhour:Number,
-
+    totalHour:Number,
+    //total:Number,
 });
+var employeeModel=mongoose.model('Employee',employeeSchema);
 
-employeeSchema.methods.totalsalary=function(){
-    console.log("total income of employee: Rs"+this.hourlyrate*this.totalhour);
-
-}
-//create a model of an object
-var employeeModel= mongoose.model("Employee",employeeSchema);
-//create an object
 var employee= new employeeModel({name:"koushik",
-email:"koushikbose628@gmail.com",
-etype:"hourly",
-hourlyrate:14,
-totalhour:16
-
+    email:"koushikbose628@gmail.com",
+    etype:"hourly",
+    hourlyrate:14,
+    totalhour:24,
 });
 
-console.log(employee);
-//console.log("total income of employee:"+employee.hourlyrate*employee.totalhour);
-employee.totalsalary();
+console.log("Total income of the employee: Rs."+employee.hourlyrate*employee.totalhour)
+
+conn.on("connected",function(){
+    console.log("Connection successfully!!!!!!!");
+});
+conn.on("disconnected",function(){
+    console.log("disConnection successfully!!!!!!!");
+});
+conn.on('error',console.error.bind(console,'connection error:'));
+//insert data into data base
+conn.once('open',function(){
+    employeedetails.employee.save(function(err,res){
+        if(err)throw error;
+        console.log(res);
+        conn.close();
+    });
+});
+
+
+
+
