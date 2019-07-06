f4ed022e2bd6825611ffcae094aa616ab5e81323
Koushik Bose <48059128+KoushikBose2019@users.noreply.github.com>
2019-07-06 19:16:02 +0530

    Insert record in Mongodb using HTML form

--- index.js
+++ index.js
@@ -17,5 +17,27 @@ res.render('index', { title: 'Employee Records',records:data });
 });
   
 });
+router.post("/",function(req,res,next){
+    
+var empdetails= new empModel({
+   name:req.body.uname,
+   email:req.body.email,
+   etype:req.body.emptype,
+   hourlyrate:req.body.ttlhr,
+   totalhour:req.body.ttlhour,
+
+});
+//used to save the data in database
+empdetails.save(function(err,res1){
+    if(err) throw err;
+    employee.exec(function(err,data){
+        if(err) throw err;        
+        res.render('index',{title:'Employee Record',records:data});   
+    });
+
+});
+
+//console.log(empdetails);
+});
 
 module.exports = router;
