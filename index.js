b79e76c061aeb22d2be151f4e8138922ede417dc
Koushik Bose <48059128+KoushikBose2019@users.noreply.github.com>
2019-07-06 20:58:33 +0530

    Filter Records From Database
    mongodb

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var router = express.Router();
 var employee=empModel.find({});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function(req, res, next) {
   
 employee.exec(function(err,data){
 if(err) throw err;
@@ -18,7 +18,7 @@ res.render('index', { title: 'Employee Records',records:data });
   
 });
 router.post("/",function(req,res,next){
-    
+
 var empdetails= new empModel({
    name:req.body.uname,
    email:req.body.email,
@@ -39,5 +39,34 @@ empdetails.save(function(err,res1){
 
 //console.log(empdetails);
 });
+router.post("/search/",function(req,res,next){
+    var name=req.body.name;
+    var email=req.body.email;
+    var EmployeeType=req.body.EmployeeType;
 
+    if(name!=''&& email!='' && EmployeeType!=''){
+      //search data from database using condition
+        var filterparameter={ $and:[{ name:name},
+        {$and:[{email:email},{etype:EmployeeType}]}
+        ]
+
+        }
+
+    }else if{(name!=''&& email!='' && EmployeeType!=''){
+      
+        var filterparameter={ $and:[{ name:name},
+        {$or:[{email:email},{etype:EmployeeType}]}
+        ]
+    }else{
+        var filterparameter={}
+    }
+    var Filteremployee=empModel.find(filterparameter);
+
+    employee.exec(function(err,data){
+     if(err) throw err;
+     res.render('index',{title:"Employee record",records:data});
+
+});
+});
 module.exports = router;
+
